3ef1dc53adfede407af0b79095b70ea733a033f0
Vitória Rodrigues Ferreira <70407409+Vitoria-Rodrigues@users.noreply.github.com>
2025-05-26 16:21:45 -0300

    Cadastrando produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -23,11 +23,20 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        try{
+        conn = new conectaDAO().connectDB();
+        String sql = "INSERT INTO produtos(nome, valor, status) VALUES (?,?,?)";
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+        prep.execute();
         
+        JOptionPane.showMessageDialog(null, "Cadastrado com sucesso");
         
-        //conn = new conectaDAO().connectDB();
-        
-        
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Erro no cadastro");
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
