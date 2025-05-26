035054ac4d8da8d74f866ea9528c50ba805a902d
Vitória Rodrigues Ferreira <70407409+Vitoria-Rodrigues@users.noreply.github.com>
2025-05-26 19:06:35 -0300

    Adicionando botao de venda

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -32,8 +32,6 @@ public class ProdutosDAO {
         prep.setString(3, produto.getStatus());
         prep.execute();
         
-        JOptionPane.showMessageDialog(null, "Cadastrado com sucesso");
-        
         } catch(Exception e){
             JOptionPane.showMessageDialog(null, "Erro no cadastro");
         }
@@ -63,8 +61,24 @@ public class ProdutosDAO {
         return listagem;
     }
     
-    
-    
-        
+    public void venderProduto(int id){
+
+        try{
+
+            conn = new conectaDAO().connectDB();
+            String sql = " UPDATE produtos SET status = 'Vendido' where id = ?";
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1,id);
+            prep.executeUpdate();
+
+             JOptionPane.showMessageDialog(null, "Produto vendido com sucesso!");
+
+        } catch (Exception e){
+
+        JOptionPane.showMessageDialog(null, "Erro ao Vender");
+        }
+
+    }
+         
 }
 
